0db5f268e8adddf6d9cb4880ea982fb0272645ac
Ömer Faruk <72097255+omerfbasaran@users.noreply.github.com>
2021-01-06 03:58:51 +0300

    Arithmetic

---
+++ Pointers-Arithmetic/Pointers-Arithmetic.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+int main()
+{
+    char byte;
+    short short_integer;
+    int integer;
+
+    char *pbyte;
+    short* pshort_integer;
+    int *pinteger;
+
+    pbyte = &byte;
+    pshort_integer = &short_integer;
+    pinteger = &integer;
+
+    printf("sizeof char: %d\n", sizeof(byte));
+    printf("sizeof short: %d\n", sizeof(short_integer));
+    printf("sizeof integer: %d\n", sizeof(integer));
+
+    printf("char adres: %d\n", pbyte);
+    printf("short adres: %d\n", pshort_integer);
+    printf("integer adres: %d\n", pinteger);
+
+    pbyte++;
+    pshort_integer++;
+    pinteger++;
+
+    printf("char adres: %d\n", pbyte);
+    printf("short adres: %d\n", pshort_integer);
+    printf("integer adres: %d\n", pinteger);
+
+}
